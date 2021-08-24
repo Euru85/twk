7e61f5a732d80763e186602199cc6aa3f1b06554
Adam Gajak <adam.gajak@gmail.com>
2021-08-24 21:29:09 +0200

    Failsafe

---
+++ src/main/java/pl/lodz/p/it/spjava/e11/twk/dto/AccountDTO.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.spjava.e11.twk.dto;
+
+import pl.lodz.p.it.spjava.e11.twk.model.Account;
+
+/**
+ *
+ * @author Adam
+ */
+public class AccountDTO {
+
+    private Long id;
+    private String login;
+    private Boolean active;
+
+    public AccountDTO() {
+    }
+    
+    public AccountDTO(Long id, String login, Boolean active) {
+        this.id = id;
+        this.login = login;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+}
